1b19b9bbdff5b461bfa84b260e89261b21263894
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-26 12:54:09 -0700

    Completed initial configuration

---
+++ subsequent-deployment-test/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Subsequent Deployment Test</h1>
+      <h2>This is a test.</h2>
+      <h3>This is only a test.</h3>
+    </div>
+  );
+}
+
+export default App;
